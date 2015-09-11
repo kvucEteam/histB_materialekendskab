af03b15dd4008f81b59cc9e59be8da8bc55661d0
Thomas <kvucthan@gmail.com>
2015-09-11 14:26:24 +0200

    Vi har valgt at navngive objektet til "materialekendskab"

--- kildekendskab.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function() {
-
-	// $('.carousel').carousel({  
-	//     interval: false; // This deactivates the autoslide. NOTE: This does not work - set data-interval="false" in the .carousel-class tag.
-	// }); 
-}) 
---
+++ materialekendskab.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+
+	// $('.carousel').carousel({  
+	//     interval: false // This deactivates the autoslide. NOTE: This does not work - set data-interval="false" in the .carousel-class tag.
+	// }); 
+}) 
