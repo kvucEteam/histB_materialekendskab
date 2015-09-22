b7636d52f2b060dd5b6cc3bea94cc869af763253
Thomas <kvucthan@gmail.com>
2015-09-22 15:45:01 +0200

    Bug ang. score ved flere rigtige svar som skal rettes

--- materialekendskab.js
+++ materialekendskab.js
@@ -183,8 +183,11 @@ function userInterfaceChanger(jsonData){
         console.log("userInterfaceChanger - questionId: " + questionId + ", nextQuestionId: " + nextQuestionId);
         CurrentQuestionId = nextQuestionId;
 
-        $(".btnContainer").hide();
-        $("#btnContainer_"+CurrentQuestionId).show();
+        $("#header").text(jsonData[CurrentQuestionId].userInterface.header);        // Shows the next heading.
+        $("#subHeader").text(jsonData[CurrentQuestionId].userInterface.subHeader);  // Shows the next subheading.
+
+        $(".btnContainer").hide();                                                  // Hides all the button containers.
+        $("#btnContainer_"+CurrentQuestionId).show();                               // Shows the next button container.
     });
 
     // $(document).on('click', ".glyphicon-chevron-right", function(event) {
@@ -194,8 +197,11 @@ function userInterfaceChanger(jsonData){
         console.log("userInterfaceChanger - questionId: " + questionId + ", nextQuestionId: " + nextQuestionId);
         CurrentQuestionId = nextQuestionId;
 
-        $(".btnContainer").hide();
-        $("#btnContainer_"+CurrentQuestionId).show();
+        $("#header").text(jsonData[CurrentQuestionId].userInterface.header);        // Shows the previous heading.
+        $("#subHeader").text(jsonData[CurrentQuestionId].userInterface.subHeader);  // Shows the previous subheading.
+
+        $(".btnContainer").hide();                                                  // Hides all the button containers.
+        $("#btnContainer_"+CurrentQuestionId).show();                               // Shows the previous button container.
     });
 }
 
@@ -203,8 +209,10 @@ function userInterfaceChanger(jsonData){
 function countCorrectAnswers(jsonData){
 	correct_total = 0;
 	error_total = 0;
+    var error_displayed_total = 0;
+    var numOfQuestions = 0;
 	for (k in jsonData){
-		var correct = 0; var error_missed = 0; var error_wrong = 0;
+		var correct = 0; var error_missed = 0; var error_wrong = 0; var error_displayed = 0;
 	    var answerArray = jsonData[k].quizData.correctAnswer;
 	    var numOfSrudentAnswers = $("#btnContainer_"+k+" > .btnPressed").length;
 	    var numOfCorrectAnswers = answerArray.length;
@@ -217,19 +225,69 @@ function countCorrectAnswers(jsonData){
 	    	error_wrong += numOfSrudentAnswers - (correct + error_missed); // Counts all the wrong answers chosen by the student. 
 	    	error_wrong = (error_wrong < 0) ?  0 : error_wrong;
 
-		    console.log("XXX - CurrentQuestionId: " + CurrentQuestionId + 
+		    console.log("countCorrectAnswers - CurrentQuestionId: " + CurrentQuestionId + 
 		    	"\nnumOfSrudentAnswers: " + numOfSrudentAnswers + ", numOfCorrectAnswers: " + numOfCorrectAnswers + 
 		    	"\ncorrect: " + correct  + ", error_missed: " + error_missed  + ", error_wrong: " + error_wrong);
+
+            if ($("#btnContainer_"+k+" > .btnPressed").length > 0){
+                if ($("#btnContainer_"+k+" > .StudentAnswer").not(":eq("+answerArray[n]+")").hasClass("btnPressed"))
+                    error_displayed += $("#btnContainer_"+k+" > .StudentAnswer").not(":eq("+answerArray[n]+")").length;
+                //     ++error_displayed;
+            }
 		}
 
 	    correct_total += correct;
-	    error_total += error_wrong + error_missed;
+	    error_total += error_wrong + error_missed - correct;
+        error_displayed_total += error_displayed;
+
+        ++numOfQuestions;
 	}
-	console.log("countCorrectAnswers - correct_total: " + correct_total + ", error_total: " + error_total);
+
+    $(".QuestionCounter").text(correct_total+'/'+numOfQuestions);
+    $(".ErrorCount").text(error_displayed_total);
+	console.log("countCorrectAnswers - correct_total: " + correct_total + ", error_total: " + error_total + ", error_displayed_total: " + error_displayed_total);
+}
+
+
+function giveFeedback(jsonData, questionNum){
+    var HTML = "<h2>Feedback</h2>";
+    var questionArray = jsonData[questionNum].userInterface.btn;
+    var feedbackArray = jsonData[questionNum].quizData.feedbackData; 
+    for (var n in questionArray){
+        if ($("#btnContainer_"+questionNum+" > .StudentAnswer:eq("+n+")").hasClass("btnPressed"))
+            HTML += "<p>"+feedbackArray[n]+"</p>";
+    }
+
+    console.log("giveFeedback - CurrentQuestionId: " + ", HTML: " + JSON.stringify(HTML));
+
+    UserMsgBox("body", HTML);
+}
+
+
+// BLIVER PT IKKE BRUGT:
+function ShowStudentScore(Use_UserMsgBox){
+    var HTML = '';  
+
+    if (Use_UserMsgBox) 
+        // UserMsgBox("body", "Du klarede det med " + TotScoreObj.TotNumOfWrongAnswers + " fejl Se resultaterne her <br/>");
+        UserMsgBox("body", "<span class='feedbackbox_txtstyle_overskrift'>Flot</span><br/>Du har lavet "+MaxNumOfElements+" opgaver korrekt. <br/> Du havde " + TotScoreObj.NewTotNumOfWrongAnswers + ' fejl undervejs. <br/><br/>Klik på "Prøv igen" knappen for at løse '+MaxNumOfElements+' nye opgaver.');
+    else
+        $(".ShowStudentScore").html( HTML );
+
+    // Update numbers:
+    $(".ScoreAttempts").text( TotScoreObj.TotNumOfAttempts ); 
+    $(".ScoreCorrect").text( TotScoreObj.TotNumOfCorrectAnswers );
+    $(".ScoreFail").text( TotScoreObj.TotNumOfWrongAnswers );
+    $(".ScoreStat").text( (TotScoreObj.TotNumOfCorrectAnswers/(TotScoreObj.TotNumOfAttempts + ReturnTotNumOfAnswers("#") - ReturnNumOfQuestions() )*100).toFixed(2) + "%" ); 
+
+    if (Use_UserMsgBox) 
+        return 0;
 }
 
 
 function CheckStudentAnswers(jsonData){
+
+    // This is a bad solution - a better one would be to let CSS handle the color-changes...
     $(document).on('click', ".StudentAnswer", function(event) {
     	event.preventDefault(); // Prevents sending the user to "href". 
         $(this).toggleClass("btnPressed");
@@ -244,27 +302,9 @@ function CheckStudentAnswers(jsonData){
     $(document).on('click', ".checkAnswer", function(event) {
         event.preventDefault(); // Prevents sending the user to "href".
 
-        var correct = 0; var error_missed = 0; var error_wrong = 0;
-        var answerArray = jsonData[CurrentQuestionId].quizData.correctAnswer;
-        var numOfSrudentAnswers = $("#btnContainer_"+CurrentQuestionId+" > .btnPressed").length;
-        var numOfCorrectAnswers = answerArray.length;
-        for (var n in answerArray){
-        	if ($("#btnContainer_"+CurrentQuestionId+" > .StudentAnswer:eq("+answerArray[n]+")").hasClass("btnPressed"))
-        		correct++;   // Counting correct answers.
-        	else
-        		error_missed++;  // Counting missed correct answers, eg. if there are two or more correct answers and the student does not answer all of the answers, then this counter counts the missed correct answers.
-        }
-        error_wrong = numOfSrudentAnswers - (correct + error_missed); // Counts all the wrong answers chosen by the student. 
-        error_wrong = (error_wrong < 0) ?  0 : error_wrong;
-
-        console.log("CheckStudentAnswers - CurrentQuestionId: " + CurrentQuestionId + 
-        	"\nnumOfSrudentAnswers: " + numOfSrudentAnswers + ", numOfCorrectAnswers: " + numOfCorrectAnswers + 
-        	"\ncorrect: " + correct  + ", error_missed: " + error_missed  + ", error_wrong: " + error_wrong);
-
-        // $(".QuestionCounter").text(correct_total+'/'+numOfQuestions);
-        // $(".ErrorCount").text(error_total);
-
         countCorrectAnswers(jsonData);
+
+        giveFeedback(jsonData, CurrentQuestionId);
     });
 }
 
@@ -282,15 +322,47 @@ function ReturnAjaxData(Type, Url, Async, DataType) {
             // console.log("HowWhyData: " + HowWhyData);
         }
     }).fail(function() {
-        alert("Ajax failed to fetch data");
+        alert("Ajax failed to fetch data - the requested quizdata might not exist...");
     });
+}
+
+
+function ReturnURLPerameters(UlrVarObj){
+    var UrlVarStr = window.location.search.substring(1);
+    console.log("ReturnURLPerameters - UrlVarStr: " + UrlVarStr);
+    var UrlVarPairArray = decodeURIComponent(UrlVarStr).split("&");  // decodeURIComponent handles %26" for the char "&" AND "%3D" for the char "=".
+    console.log("ReturnURLPerameters - UrlVarPairArray: " + UrlVarPairArray);
+    for (var i in UrlVarPairArray){
+        var UrlVarSubPairArray = UrlVarPairArray[i].split("=");  // & = %3D
+        if (UrlVarSubPairArray.length == 2){
+            UlrVarObj[UrlVarSubPairArray[0]] = UrlVarSubPairArray[1];
+        }
+    }
+    console.log("ReturnURLPerameters - UlrVarObj: " + JSON.stringify( UlrVarObj ));
+    return UlrVarObj;
+}
+
+
+
+// TEST URL:
+// file:///Users/THAN/main-gulp-folder/objekter/kemi_drag/builds/development/index.html?pn=1&dm=1    NOTE: 0 = false, 1 = true
+// file:///Users/THAN/main-gulp-folder/objekter/kemi_drag/builds/development/index.html?pn=1&dm=0    NOTE: 0 = false, 1 = true
+function SetProgramPerameter(UlrVarObj, file){
+    if (UlrVarObj.hasOwnProperty("file") && ((1 <= parseInt(UlrVarObj["file"])) || (parseInt(UlrVarObj["file"]) <= 10))) file = UlrVarObj["file"];  // PrincipleNum  =  pn
+    console.log("SetProgramPerameter - ReturnURLPerameters - Level: " + Level ); 
 }
 
 $(document).ready(function() {
 // $(window).load(function() {
 
+    var UlrVarObj = {"file" : ""};
+    UlrVarObj = ReturnURLPerameters(UlrVarObj);
+    // SetProgramPerameter(UlrVarObj);
+    console.log("UlrVarObj: " + JSON.stringify(UlrVarObj) );
+
+
 	// ReturnAjaxData("GET", "json/QuizData.json", false, "json");
-	ReturnAjaxData("GET", "json/QuizData2.json", false, "json");
+	ReturnAjaxData("GET", "json/QuizData"+UlrVarObj.file+".json", false, "json");
 
 	// returnCarouselHtml(0, jsonData);  // TEST
 
@@ -303,8 +375,13 @@ $(document).ready(function() {
 
     console.log("jsonData: " + JSON.stringify(jsonData) );
 
-    $(".btnContainer").hide();
-    $("#btnContainer_"+0).show();
+    $("#header").text(jsonData[0].userInterface.header);   // Shows the initial heading.
+    $("#subHeader").text(jsonData[0].userInterface.subHeader);    // Shows the initial subheading.
+
+    $(".btnContainer").hide();      // Hides all button containers.
+    $("#btnContainer_"+0).show();   // Shows the first button container.
+
+    $(".QuestionCounter").text(correct_total+'/'+jsonData.length);   // Counts the initial number of correctly answered questions and total number questions and displays them.
 
     CheckStudentAnswers(jsonData);
 
